909451a3841e04f01926b6b4672d802d473f33ad
dengsf <29675159@qq.com>
2020-11-07 09:41:41 +0800

    add .

--- listen6/access/main.go
+++ listen6/access/main.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/gostudy/listen6/calc"
 )
 
-func main(){
+func main() {
 	s1 := 100
 	s2 := 200
-	sum := calc.Add(s1,s2)
-	fmt.Printf("sum=%d\n",sum)
+	sum := calc.Add(s1, s2)
+	fmt.Printf("sum=%d\n", sum)
 }
